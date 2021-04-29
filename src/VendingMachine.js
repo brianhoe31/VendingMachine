65735f199393ac17f6aa67f8b2c393de8e6abcfb
Brian <brianhoe31@gmail.com>
2021-04-29 11:50:42 -0700

    First commit of Vending Machine app

---
+++ src/VendingMachine.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class VendingMachine extends Component{
+    render(){
+        return(
+            <div>
+                This is vending machine
+            </div>
+        )
+    }
+}
+
+export default VendingMachine;
